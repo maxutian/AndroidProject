9a42dba6f2626bfff1f4117d0a011b1dc321efbc
maxutian <maxutian95@gmail.com>
2017-07-06 19:01:07 +0800

    changeFrame

--- app/src/main/java/com/demo/mrma/demo/MainActivity.java
+++ app/src/main/java/com/demo/mrma/demo/MainActivity.java
@@ -1,6 +1,5 @@
 package com.demo.mrma.demo;
 
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,28 +7,29 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FloatingActionButton cameraButton;
+//    private FloatingActionButton cameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.show_photo);
         init();
         allClick();
     }
 
     public void init () {
-        cameraButton = (FloatingActionButton) findViewById(R.id.cameraButton);
+//        cameraButton = (FloatingActionButton) findViewById(R.id.cameraButton);
     }
 
     public void allClick () {
 
 //      cameraButton clickEvent
-        cameraButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "haha", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        cameraButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "haha", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
     }
 }
