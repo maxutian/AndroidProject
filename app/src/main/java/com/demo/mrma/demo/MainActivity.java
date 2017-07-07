1de9a929cdf364741deae067455d7ee188bcbc7e
maxutian <maxutian95@gmail.com>
2017-07-07 11:41:04 +0800

    buttons

--- app/src/main/java/com/demo/mrma/demo/MainActivity.java
+++ app/src/main/java/com/demo/mrma/demo/MainActivity.java
@@ -5,26 +5,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.github.clans.fab.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
-//    private FloatingActionButton cameraButton;
+    private FloatingActionButton recovery,frontCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.show_photo);
+        setContentView(R.layout.activity_main);
         init();
         allClick();
     }
 
     public void init () {
-//        cameraButton = (FloatingActionButton) findViewById(R.id.cameraButton);
+//        recovery = (FloatingActionButton) findViewById(R.id.recovery_button);
+//        frontCamera = (FloatingActionButton) findViewById(R.id.front_camera_button);
     }
 
     public void allClick () {
 
-//      cameraButton clickEvent
-//        cameraButton.setOnClickListener(new View.OnClickListener() {
+//      recovery clickEvent
+//        frontCamera.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
 //                Toast.makeText(MainActivity.this, "haha", Toast.LENGTH_SHORT).show();
